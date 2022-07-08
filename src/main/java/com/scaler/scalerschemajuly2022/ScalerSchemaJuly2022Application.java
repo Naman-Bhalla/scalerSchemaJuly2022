81c4efb9dd42880752b7ce8e2bd579fb9a188f92
Naman Bhalla <namanbhalla1998@gmail.com>
2022-07-08 23:49:15 +0530

    Initial commit

---
+++ src/main/java/com/scaler/scalerschemajuly2022/ScalerSchemaJuly2022Application.java
@@ -0,0 +1,13 @@
+package com.scaler.scalerschemajuly2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalerSchemaJuly2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalerSchemaJuly2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/scalerschemajuly2022/ScalerSchemaJuly2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.scalerschemajuly2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalerSchemaJuly2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
